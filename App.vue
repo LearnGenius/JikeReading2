a166312a99d2eb2f701144d011e09db6f47abbb8
Lee <learngenius@outlook.com>
2023-06-18 10:12:05 +0800

    配置基本页面图标

--- App.vue
+++ App.vue
@@ -14,4 +14,5 @@
 
 <style>
 	/*每个页面公共css */
+	@import url("//at.alicdn.com/t/c/font_4119616_85797o3ky5e.css");
 </style>
--- pages/homePage/homePage.vue
+++ pages/homePage/homePage.vue
@@ -1,6 +1,8 @@
 <template>
-	<view>
-		homepage
+	<view class="outView">
+		<view class="readingNowBox">
+			正在读...
+		</view>
 	</view>
 </template>
 
@@ -18,5 +20,28 @@
 </script>
 
 <style>
+	/* 总View */
+	.outView{
+		margin: 20rpx;
+	}
+	
+	/* 正在读标题 */
+	.readingNowBox{
+		
+	}
+	/* 最近的阅读列表 */
+	.reacentBooksBox{
+		
+	}
+	
+	/* 想读列表 */
+	.wantedReadBox{
+		
+	}
+	
+	/* 阅读目标 */
+	.readingGoalBox{
+		
+	}
 
 </style>
