553c67020d4989198f0e1c7fdbd8ea69f0be284f
Lee <learngenius@outlook.com>
2023-06-18 09:48:58 +0800

    搭建基本界面

---
+++ pages/booksLibrary/booksLibrary.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		booksLibrary
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
